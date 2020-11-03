7ad38f505e6944adb8ece820b71c0e77f2925902
ccatina <hungaries7@gmail.com>
2020-11-03 17:19:27 +0300

    memset is done!

---
+++ .idea/ft_memset.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccatina <ccatina@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/03 17:02:19 by ccatina           #+#    #+#             */
+/*   Updated: 2020/11/03 17:16:46 by ccatina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib>
+
+void    *memset(void *buf, int ch, size_t count){
+    size_t i;
+
+    i = 0;
+    while (i < count){
+        ((unsigned char*)buf)[i] = (unsigned char)ch;
+        i++;
+    }
+    return buf;
+}
